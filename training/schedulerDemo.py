37bbec4d319835868a7df8d5cb7a18f4261e5a1d
jiec137 <revinchen21@gmail.com>
2024-03-18 23:46:54 -0400

    Add files via upload

---
+++ training/schedulerDemo.py
@@ -0,0 +1,15 @@
+
+class LRScheduler(LearningRateSchedule):
+    def __init__(self, d_model, warmup_steps=4000, **kwargs):
+        super(LRScheduler, self).__init__(**kwargs)
+ 
+        self.d_model = cast(d_model, float32)
+        self.warmup_steps = warmup_steps
+ 
+    def __call__(self, step_num):
+ 
+        # Linearly increasing the learning rate for the first warmup_steps, and decreasing it thereafter
+        arg1 = step_num ** -0.5
+        arg2 = step_num * (self.warmup_steps ** -1.5)
+ 
+        return (self.d_model ** -0.5) * math.minimum(arg1, arg2)
